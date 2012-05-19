185529243c08ac7353d3f4b68c579c0668c02105
majorye <major.yezhouquan@gmail.com>
2012-05-19 19:57:25 +0800

    modify the code style, remove the useless code, make it as jQuery plugin, just depend on jQuery

--- logging.js
+++ logging.js
@@ -1,122 +1,112 @@
 /**
-*@class ali.getLogger
-*@name   ali.getLogger
+*@class jQuery.getLogger
+*@name   jQuery.getLogger
 *@author   <a href="mailto:zhouquan.yezq@alibaba-inc.com">Zhouquan.yezq</a>
-*@description ali.getLogger will return a logger instance, and it already mask the console
-* script issue on lower browser version like IE6. and for the IE6 or other lower version, we will
-* support it logger later. 
+*@description jQuery.getLogger will return a logger instance, and it already mask the console
+*script issue on lower browser version like IE6. and for the IE6 or other lower version, we use the log
+monitor to show the log .
  <h2>How to use?<h2>
 <pre>
-  var mylog=ali.getLogger('ali.openpf.isv.register');
+  var mylog=jQuery.getLogger('com.company.project.moduleName');
   mylog.log('this is the log');
 </pre>
-or
- * <pre>
- * ali.defineClass('ali.openpf.isv.register',MySuperclass, ali.PubSub,function(KLASS,instance){
- *     KLASS.getName=function(){
- *         KLASS.logger.log('this is the static method');
- *     };
- *     instance.getName=function(){
- *        this.logger.log('this is the instance method');
- *     };
- * });
- * </pre>
- or
-  * <pre>
-  ali.defineClass('ali.openpf.isv.register',MySuperclass, ali.PubSub,{
-  print: function(){
-    this.logger.log('this is the print method');
-  },
-  initEl:function() {
-      this.logger.log('this is the initEl method');
-  }
-  });
-  </pre>
 */
 (function($){
-   //the Logger constructor
-   $.namespace('ali.uxcore.util'); 
-   ali.uxcore.util.Logger=function(){};
-   ali.uxcore.util.Logger.level=-1; //default level
-   ali.uxcore.util.Logger.setLevel=function(level){//set logger level to filter the logger , so just show the logger level you focus.
-     ali.uxcore.util.Logger.level=level;
+   //the _log constructor
+   
+   var _log=function(){};
+   _log.level=4; //default level, show all the log,if you want to close the log, just set log level as -1
+   _log.cacheSize=1000;
+   _log.setLevel=function(level){//set logger level to filter the logger , so just show the logger level you focus.
+     _log.level=level;
    };
    
    var methods = [ "error", "warn", "info", "debug", "log"];//0-4 level
    
-   $.extend(ali.uxcore.util.Logger.prototype, {
-     level:ali.uxcore.util.Logger.level,//default level is 4, so all the log will be print out.,
-     setEnableLevel: function(level) {
-       if(level>4 || level<0) {
-         this.error(['wrong level setting. level should be 0-4, the int type,you set ',level,", so stupided."].join(''));
-       }
-       this.level=parseInt(level);
-     },
+   $.extend(_log.prototype, {
+    /***
+          *  @name jQuery.getLogger#enabled
+          * @function
+          */
      enabled: function(lev) {
-       if(lev>this.level) {
+       if(lev>_log.level ) {
          return false;
        }
        return true;
      },
+     /***
+          *  @name jQuery.getLogger#doFilter
+          * @function
+          */
+	 doFilter: function(log) {
+	   if(_log.filter && !log.test(_log.filter)) {
+	     return false
+	   }
+	   return true;
+	 },
      name: function() {
        return this._name;
      },
      /***
-          *  @name ali.getLogger#log
+          *  @name jQuery.getLogger#log
           * @function
           */
      log: function() {
         this._log(4, arguments);
      },
      /***
-          *  @name ali.getLogger#debug
+          *  @name jQuery.getLogger#debug
           * @function
           */
      debug: function() {
         this._log(3, arguments);
      },
      /***
-          *  @name ali.getLogger#info
+          *  @name jQuery.getLogger#info
           * @function
           */
      info: function() {
         this._log(2, arguments);
      },
     /***
-          *  @name ali.getLogger#warn
+          *  @name jQuery.getLogger#warn
           * @function
           */
      warn: function() {
         this._log(1, arguments);
      },
      /***
-          *  @name ali.getLogger#error
+          *  @name jQuery.getLogger#error
           * @function
           */
      error: function() {
         this._log(0, arguments);
      },
      _handler: function(level, name, msg){
-
         var method=methods[level];
         msg=[[method,name+" |"].join(" | ")].concat(Array.prototype.slice.call(msg));
-             if(!ali.uxcore.util.Logger.logPool){
-               ali.uxcore.util.Logger.logPool=[];
+             if(!_log.logPool){
+               _log.logPool=[];
              }
-             ali.uxcore.util.Logger.logPool.push(msg.join(''));
-             if( ali.monitor && ali.monitor.trunOn ){ //$.browser.msie &&
-               ali.monitor.appendMessage(msg.join(''));
+			 if(_log.logPool.length===_log.cacheSize){
+			    //if the cache log more than cacheSize , then remove the  previous first one.
+			    _log.logPool=_log.logPool.slice(1);
+			 }
+             _log.logPool.push(msg.join(''));
+             if( this.monitor && this.monitor.trunOn ){ //$.browser.msie &&
+               this.monitor.appendMessage(msg.join(''));
              }
-       if(self.console && self.console.error){
-           if(console.log.apply){//IE8 do not work on this way. undefined
+       if(self.console && self.console.error) {
+           if(console.log.apply) {//IE8 do not work on this way. undefined
               console[method].apply(console, msg);       
-           }else{
+           }else {
               console[console[method]?method:'log'](msg);
            }
        }
      },
+	 
     _log: function(level, msg) {
-      if (this.enabled(level)) {
+      if (this.enabled(level) && this.doFilter(msg)) {
          this._handler(level,this.name(),msg);
       }
     }
@@ -124,14 +114,21 @@ or
    });
    
    var logs={};//logs container
-   ali.uxcore.util.getLogger= function(name) {
+   //extend this getLoggger method as jQuery method
+   $.extend({
+     getLogger: function(name) {
        if (!logs[name]) {
-          logs[name] = new ali.uxcore.util.Logger(name);
+          logs[name] = new _log(name);
           logs[name]._name=name;
         }
         return logs[name];
-   };
-   //short cut
-   ali.logger=ali.uxcore.util.Logger;
-   ali.getLogger=ali.uxcore.util.getLogger;
+     },
+	 setLogLevel: function(level) {
+	   _log.level=level;
+	 },
+	 setLogFilter: function(filter) { // filter should be RegExp pattern
+	   _log.filter=filter;
+	 }
+   });   
+   
 })(jQuery);
--- monitor.js
+++ monitor.js
@@ -1,61 +1,59 @@
 /**
- *@class ali.monitor
- *@name   ali.monitor
- *@author   <a href="mailto:zhouquan.yezq@alibaba-inc.com">Zhouquan.yezq</a>
- *@description
+ *@author   <a href="mailto:major.yezhouquan@gmail.com">Zhouquan.yezq</a>
+ *@description  the monitor.js depend on the logging.js, that want to solve the log under ie6 etc lower browser do not show up.
  */
 (function($) {
-  //the Logger constructor
-  $.namespace('ali.monitor');
-  var KLASS = ali.monitor;
-  KLASS.MONITOR_PAGE = "monitor.html"; //this html file ,you should put together with your project file.   
-  KLASS.getName = function() {
-    return "iLogger"
-  };
-  ali.monitor.trunOn = false;
-  window.childOpen = false;
-  $(document).keydown(function(e) {
-    if (e.ctrlKey && e.altKey && e.keyCode == 76) { //ctrl+alt+l
-      ali.monitor.trunOn = true;
-      KLASS._openWindow();
-    }
-  });
-  KLASS._openWindow = function() {
-    var url = window.location.href;
-    url = url.replace(window.location.pathname, '/' + KLASS.MONITOR_PAGE);
-    KLASS._window = window.open(url, "Monitor_", "directories=no," + "location=no," + "menubar=no," + "status=yes," + "personalbar=no," + "titlebar=yes," + "toolbar=no," + "resizable=yes," + "scrollbars=no," + "width=500," + "height=400");
-    window.childOpen = true;
-    if (this._window) {
-      window.focus();
-    }
-    window.onunload = this.UpdateChild;
-  };
 
-  KLASS.UpdateChild = function() {
-    //Only if child window is still open, set the parentOpen property
-    if (window.childOpen == true) {
-      this._window.opener=null;
-      this._window.parentOpen = false
-      KLASS._window.close();
+  //the monitor constructor and config
+  var _m={
+    MONITOR_PAGE: "monitor.html", //this html file ,you should put together with your project file.   
+	trunOn: false,
+	_openWindow: function() {
+	    var url = window.location.href;
+	    url = url.replace(window.location.pathname, '/' + this.MONITOR_PAGE);
+	    this._window = window.open(url, "Monitor_", "directories=no," + "location=no," + "menubar=no," + "status=yes," + "personalbar=no," + "titlebar=yes," + "toolbar=no," + "resizable=yes," + "scrollbars=no," + "width=500," + "height=400");
+	    window.childOpen = true;
+	    if (this._window) {
+	      window.focus();
+	    }
+	    window.onunload = this.UpdateChild;
+    },
+	UpdateChild: function() {
+	    //Only if child window is still open, set the parentOpen property
+	    if (window.childOpen == true) {
+	      //this._window.opener=null;
+	      //this._window.parentOpen = false
+	      //this._window.close();
+	    }
+    },
+	appendMessage: function(msg) {
+	    var w = this._window;
+	    if (!w || !window.childOpen) {
+	      this._openWindow();
+	      w = this._window;
+	    }
+	    if (w && w.appendMessage) {
+	      if (w.isFirstTime()) {
+	        var memory = $.getLogger().constructor.logPool;
+	        for (var i = 0; i < memory.length; i++) {
+	          w.appendMessage(memory[i]);
+	        }
+	      }
+	      w.appendMessage(msg);
+	    }
     }
   };
-  KLASS.onHotKey = function() {
-    if (this._window == null || this._window.closed) this._openWindow();
-  };
-  KLASS.appendMessage = function(msg) {
-    var w = this._window;
-    if (!w || !window.childOpen) {
-      this._openWindow();
-      w = this._window;
-    }
-    if (w && w.appendMessage) {
-      if (w.isFirstTime()) {
-        var memory = ali.logger.logPool;
-        for (var i = 0; i < memory.length; i++) {
-          w.appendMessage(memory[i]);
-        }
-      }
-      w.appendMessage(msg);
-    }
-  };
+ 
+  if($.getLogger){//if the log class exist, attend monitor to log prototype
+	  window.childOpen = false;
+	  var logPro=$.getLogger().constructor.prototype;
+	  logPro.monitor=_m;
+	  $(document).keydown(function(e) {
+	    if (e.ctrlKey && e.altKey && e.keyCode == 76) { //ctrl+alt+l
+	      _m.trunOn = true;
+	      _m._openWindow();
+	    }
+	  });
+  }
+  
 })(jQuery);
